66d547e78f24f9e6c3e3b797ea5b8cd10d0a653e
luisalemorales <luis1alemorales@gmail.com>
2026-07-27 13:30:33 -0400

    Add Excel import and export routes to URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     path('registrar-cliente/', views.registrar_cliente, name='registrar_cliente'),
+    
+    # Rutas para la importación y exportación a Excel:
+    path('exportar-excel/', views.exportar_excel, name='exportar_excel'),
+    path('importar-excel/', views.importar_excel, name='importar_excel'),
 ]
